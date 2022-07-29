b1c57704da1cebcf2795262af3e4631e4201d942
A-Lasitsa <108688643+A-Lasitsa@users.noreply.github.com>
2022-07-29 19:22:09 +0300

    Add files via upload
    SQL file

---
+++ SQLQueryProducts.sql
@@ -0,0 +1,12 @@
+/* Таблица с продуктами - Product; название продукта во всех таблицах - pName */
+declare @SQL nvarchar(max) = 'select Product.pName, category from Product left join ('
+
+select @SQL = @SQL + 'select Product.pName, '''+ t.TABLE_NAME +''' as category from Product join ['+ t.TABLE_NAME +'] on Product.pName = ['+ t.TABLE_NAME +'].pName union ' 
+From INFORMATION_SCHEMA.TABLES t
+where TABLE_NAME != 'Product'
+
+select @SQL = substring(@SQL,1,len(@SQL)-6)
+
+select @SQL = @SQL + ') as incl on Product.pName = incl.pName'
+
+exec sp_executesql @SQL
